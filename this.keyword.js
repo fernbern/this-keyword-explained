d743b7235b6a6f8c64ddfc6323a0a65134ec7439
fernbern <70728008+fernbern@users.noreply.github.com>
2021-01-11 16:36:10 -0800

    COOL

--- this.keyword.js
+++ this.keyword.js
@@ -1,2 +1,30 @@
 // explaining the this key word and all of it's different fomrs
 console.log(this);
+const test = {
+    prop: 42,
+    func: function() {
+        return this.prop;
+    },
+};
+console.log(test.func());
+var person = {
+    firstName: 'John',
+    lastName: 'Done',
+    id: 5000,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+        return this.firstName + " " + this.lastName;
+    }
+};
+document.getElementById('demo').innerHTML =
+person.fullName();
+var person = {
+    firstName: 'John',
+    lastName: 'Done',
+    id: 5000,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+document.getElementById('demo').innerHTML = 
+person.fullName();
