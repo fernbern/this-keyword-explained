cdfe8c5224a4239c5b4d35f9ac08d92f168d3c58
fernbern <70728008+fernbern@users.noreply.github.com>
2021-01-29 20:04:12 -0800

    update

--- this.keyword.js
+++ this.keyword.js
@@ -36,3 +36,96 @@ var showWidth = function() {
     document.write(this.width);
 }
 showWidth();
+const counter = {
+    count: 0,
+    next: function() {
+        return ++this.count;
+    }
+};
+counter.next();
+
+counter.next();
+console.log(this === window);
+this.color = 'Red';
+console.log(window.color);
+function show() {
+    console.log(this === window);
+}
+show();
+window.show();
+'use strict';
+function show() {
+    console.log(this === undefined);
+}
+function show() {
+    'use strict';
+    console.log(this === undefined);
+    function display() {
+        console.log(this === undefined);
+    }
+    bar();
+}
+foo();
+let car = {
+    brand: 'Honda',
+    getBrand: function() {
+        return this.brand;
+    }
+}
+console.log(car.getBrand());
+let brand = car.getBrand;
+console.log(brand());
+let brand = car.getBrand.bind(car);
+console.log(brand());
+let car = {
+    brand: 'Honda',
+    getBrnad: function() {
+        return this.brand;
+    }
+}
+let bike = {
+    brnad: 'Herlet Davison'
+}
+let brand = car.getBrand.bind(bike);
+console.log(brand())
+function Car(brand) {
+    this.brand = brand;
+}
+Car.prototype.getBrand = function() {
+    return this.brand;
+}
+var car = new Car('Honda');
+console.log(car.getBrand());
+var bmw = Car('BMW');
+console.log(bmw.brand);
+function Car(brand) {
+    if(!(this instanceof Car)) {
+        throw Error('Must use the new operator to call function');
+    }
+    this.brand = brand;
+}
+function Car(brand) {
+    if(!newLength.target) {
+        throw Error('Must use the new operator to call');
+    }
+    this.brand = brand;
+}
+function getBrand(prefix) {
+    console.log(prefix + this.brand);
+}
+let honda = {
+    brand: 'Honda'
+};
+let audi = {
+    brand: 'Audi'
+};
+getBrand.call(honda, 'It is a');
+getBrand.call(audi, 'it is an ');
+let getThis = () => this;
+console.log(getThis() === window);
+function Car() {
+    this.speed = 120;
+}
+Car.prototype.getSpeed = () => {
+    return this.speed;
+}
